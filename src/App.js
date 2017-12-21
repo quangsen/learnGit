4156cb64a0ef739e26cfa3037a96c9206eff43fd
quagnsen283 <quangsen283@gmail.com>
2017-12-21 09:01:12 +0700

    create

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import logo from './logo.svg';
+import './App.css';
+import contact from './Component/contact';
+
+class App extends Component {
+    render() {
+        return (
+            <contact />
+        );
+    }
+}
+
+export default App;
---
+++ src/Components/Contact.js
@@ -0,0 +1,14 @@
+import React, {Component, PropTypes} from 'react';
+
+export default class Contact extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+
+    render() {
+        return (
+            <div>NewComponent</div>
+        );
+    }
+}
